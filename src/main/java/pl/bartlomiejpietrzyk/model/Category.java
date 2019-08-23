27f9c471930b4e7e24baf80794ac8e9b60218ec1
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-23 11:23:47 +0200

    cat up

--- src/main/java/pl/bartlomiejpietrzyk/model/Category.java
+++ src/main/java/pl/bartlomiejpietrzyk/model/Category.java
@@ -3,17 +3,17 @@ package pl.bartlomiejpietrzyk.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @Entity
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Hint> hints;
 }
