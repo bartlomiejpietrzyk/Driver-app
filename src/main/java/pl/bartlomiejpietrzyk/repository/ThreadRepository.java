d8d45d02f4d94c82a57da0b4fc9a3985a278da3c
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-25 01:35:14 +0200

    annotation add

--- src/main/java/pl/bartlomiejpietrzyk/repository/ThreadRepository.java
+++ src/main/java/pl/bartlomiejpietrzyk/repository/ThreadRepository.java
@@ -1,7 +1,9 @@
 package pl.bartlomiejpietrzyk.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ThreadRepository extends JpaRepository<Thread, Long> {
     Thread findByTitle(String title);
 }
