c7140ffe2c5332b80635b15326f4b938276764d6
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-08-25 18:39:37 +0200

    role rest controller created

---
+++ src/main/java/pl/bartlomiejpietrzyk/controller/rest/RoleRestController.java
@@ -0,0 +1,14 @@
+package pl.bartlomiejpietrzyk.controller.rest;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/role")
+@Api
+public class RoleRestController {
+
+
+
+}
